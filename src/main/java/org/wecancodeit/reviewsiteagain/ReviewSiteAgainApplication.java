affcf5b66e29aab2a3dc51777a620ebc3d1aeabd
Thomas Lenhart <lenhartthomas1@gmail.com>
2018-11-02 14:26:55 -0400

    iInitial Commit

---
+++ src/main/java/org/wecancodeit/reviewsiteagain/ReviewSiteAgainApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.reviewsiteagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewSiteAgainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewSiteAgainApplication.class, args);
+	}
+}
