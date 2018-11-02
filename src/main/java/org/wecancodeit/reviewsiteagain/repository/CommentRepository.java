0151067e81a6f90c460170977769106b3cc3af83
Thomas Lenhart <lenhartthomas1@gmail.com>
2018-11-02 15:04:04 -0400

    extened CrudRepo

--- src/main/java/org/wecancodeit/reviewsiteagain/repository/CommentRepository.java
+++ src/main/java/org/wecancodeit/reviewsiteagain/repository/CommentRepository.java
@@ -1,5 +1,9 @@
 package org.wecancodeit.reviewsiteagain.repository;
 
-public interface CommentRepository {
+
+import org.springframework.data.repository.CrudRepository;
+import org.wecancodeit.reviewsiteagain.modle.Comment;
+
+public interface CommentRepository extends CrudRepository<Comment, Long> {
 
 }
