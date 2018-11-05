05e555dfe75e0d295373a3dd631cd3deb3ed1efb
Thomas Lenhart <lenhartthomas1@gmail.com>
2018-11-04 20:28:51 -0500

    more test and js edits

--- bin/main/static/js/app.js
+++ bin/main/static/js/app.js
@@ -33,11 +33,20 @@ addTagButton.addEventListener('click', () => {
  			.then(res => res.json())
  			.then(data => {
  				data.forEach(tag =>{
- 					p.innerHTML = `
- 						<a href = "/tag/${tag.id}">${tag.tagName}</a>
- 					` + p.innerHTML; 
- 				})
- 			})
-// Creates a new AJAX request
+ 				p.innerHTML = p.innerHTML + `
+				<a href = "/tag/${tag.id}" class="a${tag.id}">${tag.tagName}</a> <button class="button${tag.id}">X</button>	`; 
+				document.querySelector(`.button${tag.id}`).addEventListener('click', () => {
+				fetch(`/api/review/${window.location.pathname.split('/')[2]}/tags/remove`, {
+				method: 'POST',
+				body:	JSON.stringify({
+				tagName: tag.tagName
+				})
+				}).then(() => {
+				document.querySelector(`.a${tag.id}`).remove();
+				document.querySelector(`.button${tag.id}`).remove();
+			})
+		})
+	})
+})
 
 
--- src/test/java/org/wecancodeit/reviewsiteagain/repository/RepositoryTest.java
+++ src/test/java/org/wecancodeit/reviewsiteagain/repository/RepositoryTest.java
@@ -1,5 +1,54 @@
 package org.wecancodeit.reviewsiteagain.repository;
 
-public class RepositoryTest {
+	import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasItems;
+import static org.junit.Assert.assertThat;
 
+import java.util.Collection;
+
+
+	import javax.annotation.Resource;
+
+	import org.junit.Assert;
+	import org.junit.Test;
+	import org.junit.runner.RunWith;
+	import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+	import org.springframework.test.context.junit4.SpringRunner;
+	import org.wecancodeit.reviewsiteagain.repository.ReviewRepository;
+	import org.wecancodeit.reviewsiteagain.modle.Review;
+	import org.wecancodeit.reviewsiteagain.modle.Tag;
+
+	@RunWith(SpringRunner.class)
+	@DataJpaTest
+	public class RepositoryTest {
+
+
+		@Resource
+		TagRepository tagRepo;
+		
+		@Resource
+		ReviewRepository repo;
+
+		@Test
+		public void shouldReturnAllContacts() {
+			Review review = repo.save(new Review());
+			Review review2 = repo.save(new Review());
+			Iterable<Review> result = repo.findAll();
+			assertThat(result, hasItems(review2));
+		}
+
+		@Test
+		public void shouldAddATag() {
+			Tag tag1 = new Tag("tag");
+			tagRepo.save(tag1);
+			Iterable<Tag> result = tagRepo.findAll();
+			Assert.assertThat(result, hasItem(tag1));
+		}
+
+		public void shouldAddATagToReview() {
+			Tag tag1 = new Tag("tag");
+			Review review = new Review();
+			review.addTag(tag1);
+			Assert.assertThat(review.getTags(), hasItem(tag1));
+		}
 }
